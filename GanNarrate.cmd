eb787a72aead031230ee0a70858136b5d81b11aa
Kaitan1995 <renkaitan11@gmail.com>
2024-02-27 11:21:52 +0800

    Completion of GAN Narrate.

---
+++ GanNarrate.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'GanNarrate.exe' with Python DLL being found.
+set PATH=C:\Users\Kai\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\Kai\AppData\Local\Programs\Python\Python310
+set NUITKA_PYTHONPATH=C:\Users\Kai\Desktop\(Private) GAN Narrate;C:\Users\Kai\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Kai\AppData\Local\Programs\Python\Python310\lib;C:\Users\Kai\AppData\Local\Programs\Python\Python310;C:\Users\Kai\Desktop\(Private) GAN Narrate\GanNarrateEnv;C:\Users\Kai\Desktop\(Private) GAN Narrate\GanNarrateEnv\lib\site-packages
+"%~dp0GanNarrate.exe" %*
